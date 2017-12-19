ea8b91cb00e7ec8ebc009c2e70c44b5dfcb7a91f
AlexandreRab <34681266+AlexandreRab@users.noreply.github.com>
2017-12-19 15:39:55 +0100

    Ajout LangueBO

---
+++ BO/LangueBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class LangueBO
+    {
+        public int id_Langue { get; set; }
+        public bool Par_Defaut { get; set; }
+        public string Langue_intitule { get; set; }
+    }
+}
