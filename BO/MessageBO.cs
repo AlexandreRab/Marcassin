0e85358811cc989430b8b1779a32f8d50b45e36e
Unknown <jululien.gautier@laposte.net>
2017-12-19 16:20:53 +0100

    Création classe
    Utilisateur, room, message

---
+++ BO/MessageBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class MessageBO
+    {
+        public int id_Message { get; set; }
+        public string Contenu { get; set; }
+        public List<UtilisateurBO> idSources { get; set; }
+        public List<RoomBO> idRooms { get; set; }
+        public DateTime Date_ajout { get; set; }
+    }
+}
---
+++ BO/RoomBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class RoomBO
+    {
+        public int id_Room { get; set; }
+        public string Nom { get; set; }
+        public int Nb_utilisateurs { get; set; }
+    }
+}
