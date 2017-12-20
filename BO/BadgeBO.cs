a781b0a4ac0e2ebdf269caa6f9d2eee3fdbfb6ab
Unknown <jululien.gautier@laposte.net>
2017-12-20 11:21:26 +0100

    JG Class
    Badge, , langueBadge, UtilisateurBadge, UtilisateurRoom

---
+++ BO/BadgeBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class BadgeBO
+    {
+        public int id_Badge { get; set; }
+        public string Intitule { get; set; }
+        public string Image { get; set; }
+
+    }
+}
