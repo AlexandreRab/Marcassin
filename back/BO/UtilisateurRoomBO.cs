3a1372e7d8df7305e226255e92b88b1684363d38
SimonHpro <simon.huet@epsi.fr>
2018-03-26 13:56:45 +0200

    First commit
    base front

---
+++ back/BO/UtilisateurRoomBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class UtilisateurRoomBO
+    {
+        public List<UtilisateurBO> Utilisateurs { get; set; }
+        public List<RoomBO> Rooms { get; set; }
+    }
+}
