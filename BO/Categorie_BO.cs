2791d23c23586ff9a5ef6e452e5b9d687541f90a
AlexandreRab <34681266+AlexandreRab@users.noreply.github.com>
2018-03-26 10:05:10 +0200

    gerer le fait de trouver les categories globales genre informatique , comptabilité , ....et  stocker les sous cat dedans

--- BO/Categorie_BO.cs
+++ BO/Categorie_BO.cs
@@ -11,6 +11,7 @@ namespace BO
         public int id_categorie { get; set;}
         public string intitule { get; set; }
         public string categorie_mere { get; set; }
+        public List<Categorie_BO> Categorie { get; set; }
         //public List<Competance_BO> Competance { get; set; }
     }
 }
