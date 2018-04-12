31542ca6832f8187d16eb4b1df170e276d67a018
Julien GAUTIER <jululien.gautier@laposte.net>
2018-04-12 14:12:46 +0200

    DAL_juju, suppression doublons BOx

--- back/BO/LangueCompetence_BO.cs
+++ back/BO/LangueCompetence_BO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    public class CategorieLangue_BO
+    public class CompetenceLangue_BO
     {
-        public List<Categorie_BO> Categories{ get; set;}
-        public List<Langue_BO> Langues { get; set; }
+        public List<Competence_BO> Competence{ get; set;}
+        public List<Langue_BO> Langue { get; set; }
         public string Traduction { get; set; }
     }
 }
